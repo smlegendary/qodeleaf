89fd4b2609cd04c124fcf4615f97f8fd844da893
SHUBHANKAR MISHRA <112575600+smlegendary@users.noreply.github.com>
2024-06-08 16:39:09 +0530

    Create reducer.js

---
+++ reducer.js
@@ -0,0 +1,40 @@
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from './actionTypes';
+
+const initialState = {
+  todos: [],
+};
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: Date.now(),
+            content: action.payload.content,
+            completed: false,
+          },
+        ],
+      };
+    case REMOVE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload.id),
+      };
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload.id
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
